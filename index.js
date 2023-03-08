3fcfc9bf948edb6bc5034cc8c83f61b10494480a
umittengiz <umitengiz@gmail.com>
2023-03-08 16:08:10 +0300

    message object updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const { Configuration, OpenAIApi } = require('openai');
-require('dotenv').config();
+const express = require("express");
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
@@ -10,20 +10,24 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-app.post('/chat', async (req, res) => {
+app.post("/chat", async (req, res) => {
   try {
     // console.log(req.body.messages[0].content);
-    const { messages } = req.body;
+    let message = {
+      role: "user",
+    };
+    message.content = req.body;
+    // const { messages } = req.body;
     const completion = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages,
+      model: "gpt-3.5-turbo",
+      messages: [message],
     });
     const response = completion.data.choices[0].message;
     // console.log(response.content)
     res.json(response.content.split("\n\n")[1]);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
